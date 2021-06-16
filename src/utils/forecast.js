2576dadcf5a6f3d85b27d12da5ecd58d3ae07dd0
OsmanDsilva <christopher.osman10@gmail.com>
2021-06-16 10:21:39 +0530

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (lat,long,callback)=>{
+    const url = 'http://api.weatherstack.com/current?access_key=2080c73340432b5a4197978591c28df1&query='+lat+','+long+'&units=f'
+    request({url:url,json:true},(error,response)=>{
+        if(error)
+            callback('Unable to connect to weather service!',undefined)
+        else if(response.body.error)
+            callback(response.body.error.info,undefined)
+        else
+            callback(undefined,response.body.current.weather_descriptions[0]+'. It is currently '+response.body.current.temperature+' degrees but it feels like '+response.body.current.feelslike+' degrees.'
+            )
+        })
+}
+
+module.exports = forecast
